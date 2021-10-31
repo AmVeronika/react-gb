e47494e60e366e6a27674085639f0df3fc537a5c
Veronika Mikhotina <mihotinaw@gmail.com>
2021-10-31 19:10:11 +0300

    lesson2 var 1

--- src/components/message/Message.jsx
+++ src/components/message/Message.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 import './message.scss'
 
-export const Message = ({text})=> {
+
+export const Message = ({messageList}) => {
+    const messages = messageList.map(msg => {
+        return (
+            <p className={msg.author === "bot" ? 'bot' : "user"} key={Math.random()} >
+                {msg.text}
+                <span className="message__wrap-name"> {msg.author}</span>
+            </p>)
+    })
     return (
-        <p className='message-text'> Hello { text }</p>
+        <div className="message__wrap castom__scroll">
+            {messages}
+        </div>
     )
 }
