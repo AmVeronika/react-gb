d29ffd149b1c52c10be7a3e1d4d9f549b7085294
Veronika Mikhotina <mihotinaw@gmail.com>
2021-11-14 19:47:32 +0300

    lesson4 var 2

--- src/App.js
+++ src/App.js
@@ -7,22 +7,26 @@ import {Chats} from "./pages/Chats";
 import {Dialogs} from "./pages/Dialogs";
 import {Profiles} from "./pages/Profiles";
 import {Nav} from "./components/nav/Nav";
+import {Provider} from "react-redux";
+import {store} from "./store";
 
 function App() {
 
     return (
-        <BrowserRouter>
-            <div className="app">
-                <Nav/>
-                <Routes>
-                    <Route path="/" element={<Home/>} />
-                    <Route path="/chats" element={<Chats/>}/>
-                    <Route path="/chats/dialogs/:id" element={<Dialogs/>}/>
-                    <Route path="/profiles" element={<Profiles/>}/>
-                    <Route path="*" element={<Home/>}/>
-                </Routes>
-            </div>
-        </BrowserRouter>
+        <Provider store={store}>
+            <BrowserRouter>
+                <div className="app">
+                    <Nav/>
+                    <Routes>
+                        <Route path="/" element={<Home/>} />
+                        <Route path="/chats" element={<Chats/>}/>
+                        <Route path="/chats/dialogs/:id" element={<Dialogs/>}/>
+                        <Route path="/profiles" element={<Profiles/>}/>
+                        <Route path="*" element={<Home/>}/>
+                    </Routes>
+                </div>
+            </BrowserRouter>
+        </Provider>
 
     );
 }
